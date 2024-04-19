c387f6bd81809cf9542b3e2d2e634b3375bbf157
sebastianR0536 <160978409+sebastianR0536@users.noreply.github.com>
2024-04-18 20:24:29 -0500

    Create authentication.js

---
+++ src/routes/authentication.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+//const bcrypt = require("bcrypt");
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const userSchema = require("../models/user");
+router.post('/signup', async (req, res) => {
+    const { usuario, correo, clave } = req.body;
+    const user = new userSchema({
+        usuario: usuario,
+        correo: correo,
+        clave: clave
+    });
+    //user.clave = await user.encryptClave(user.clave);
+    await user.save(); //save es un método de mongoose para guardar datos en MongoDB 
+    //res.json(user);
+    res.json({
+        message: "Usuario guardado."
+    });
+});
+module.exports = router;
+//inicio de sesión
+router.post("/login", async (req, res) => {
+    // validaciones
+    const { error } = userSchema.validate(req.body.correo, req.body.clave);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    //Buscando el usuario por su dirección de correo
+    const user = await userSchema.findOne({ correo: req.body.correo });
+    //validando si no se encuentra
+    if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
+    //Transformando la contraseña a su valor original para 
+    //compararla con la clave que se ingresa en el inicio de sesión
+    /*const validPassword = await bcrypt.compare(req.body.clave, user.clave);
+    if (!validPassword)
+        return res.status(400).json({ error: "Clave no válida" });
+    */
+    const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+        expiresIn: 60 * 60 * 24, //un día en segundos
+    });
+    res.json({
+        auth: true,
+        token,
+    });
+});
